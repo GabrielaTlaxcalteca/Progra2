135c79b6c071c7f6cad0ecdd4ddd83f20c5b6c85
GabrielaTlaxcalteca <142054219+GabrielaTlaxcalteca@users.noreply.github.com>
2023-08-24 13:19:27 -0600

    Create Proyecto clase 2.cs

---
+++ Clase1/Clase1/Proyecto clase 2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase1
+{
+
+        public class Teléfono 
+    {
+        public int NETWORKTechnology;
+        public int LAUNCHAnnounced;
+        public int LAUNCHStatus;
+        public int BODYDimensions;
+        public int BODYWeight
+        public int BODYSIM
+        public int PLATFORMType
+        public int PLATFORMSize
+        public int PLATFORMResolution
+        public int PLATFORMProtection
+        public int PLATFORMOS
+        public int PLATFORMChipset
+        public int PLATFORMCPU
+        public int PLATFORMGPU
+        public int MEMORYCardSlot
+        public int MEMORYInternal
+        public int MAINCAMERA
+        public int
+        public int
+        public int
+        public int
+        public int
+        public int
+        public int
+        public int
+        public int
+        public int
+        public int
+        public int
+        public int
+
+        class Program
+    static void Main(string[] args)
+            { 
+            Teléfono Xiaomi = new Teléfono();
+            Telefono1.Marca = Xiaomi;
+            Telefono1.Precio = 330;
+            Telefono1.Peso = 204f;
+            Telefono1.RAM = 12;
+            Telefono1.Memoria = 256;
+
+            Teléfono Telefono2 = new Teléfono();
+            Telefono2.Marca = Xiaomi;
+            Telefono2.Precio = 330;
+            Telefono2.Peso = 204f;
+            Telefono1.RAM = 12;
+            Telefono1.Memoria = 256;
+
+        }
