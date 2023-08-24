9697bb249b9155ca97a9ed9a9e856bde9076a60f
GabrielaTlaxcalteca <142054219+GabrielaTlaxcalteca@users.noreply.github.com>
2023-08-24 13:18:17 -0600

    Create PistaCarreras.cs

---
+++ Clase1/CarreraAutos/PistaCarreras.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarreraAutos
+{
+    class PistaCarreras
+    {
+        public string nombre;
+        public float longitud;
+
+        public void Competir(AutoCarreras auto1, AutoCarreras auto2)
+        {
+            Console.WriteLine("Inciando carrera entre" + auto1.modelo + "y" + auto2.modelo)
+
+            if (auto1.velocidad > auto2.velocidad)
+            {
+                Console.WriteLine("Ganó" + auto1.modelo);
+            }
+
+            else
+            {
+                Console.WriteLine("Hubo empate");
+            }
+            
+   
+    }
+}
